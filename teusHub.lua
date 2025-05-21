2062b196e0e659f1d486b6deebcfc1d73b036421
Max7 <levygabrieel2022@gmail.com>
2025-05-21 14:37:00 -0300

    Create teusHub.lua

---
+++ teusHub.lua
@@ -0,0 +1,9 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/Fisch.lua"))()
+
+task.wait(2)
+
+for _, v in pairs(game:GetDescendants()) do
+    if v:IsA("TextLabel") and v.Text == "Fisch Hub" then
+        v.Text = "Max hub" -- aqui você bota o título novo
+    end
+end
